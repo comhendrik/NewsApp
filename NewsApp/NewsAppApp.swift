bb708d04c138996422c111a0004a4b1016fc8338
comhendrik <hendrik0807@icloud.com>
2022-07-22 18:11:15 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Hendrik Steen on 22.07.22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
